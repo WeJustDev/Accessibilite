997f3138646ce728dbefb11af47033b67b62b424
FROMAGEAT JULIE <julie.fromageat2@etu.unistra.fr>
2024-09-29 21:56:33 +0200

    creation page table

---
+++ components/table.php
@@ -0,0 +1,250 @@
+  <table role="table"
+      class="min-w-full table-auto border-collapse" id="tableNuitees">
+      <caption class="text-lg font-semibold my-4 text-center">
+          Nuitées de la saison d’été 2022 et évolution entre 2019 et 2022
+          par département et type d’hébergement
+      </caption>
+      <aria-describedby class="sr-only">loong tableau haha</aria-describedby>
+      <thead class="bg-gray-200">
+          <colgroup>
+              <col>
+          </colgroup>
+          <colgroup span="2"></colgroup>
+          <colgroup span="2"></colgroup>
+          <colgroup span="2"></colgroup>
+          <colgroup span="2"></colgroup>
+      <tbody>
+          <!-- 1e ligne du tableau -->
+          <tr>
+              <td class="border px-4 py-2" rowspan="2"></td>
+              <th id="hotels" class="border px-4 py-2" colspan="2" scope="colgroup">Hôtels - avril-septembre&nbsp;2022</th>
+              <th id="camping" class="border px-4 py-2" colspan="2" scope="colgroup">Camping - avril-septembre&nbsp;2022</th>
+              <th id="autre" class="border px-4 py-2" colspan="2" scope="colgroup">Autres hébergements collectifs de tourisme -
+                  avril-septembre&nbsp;2022</th>
+              <th id="total" class="border px-4 py-2" colspan="2" scope="colgroup">Total - avril-septembre&nbsp;2022</th>
+          </tr>
+          <tr>
+              <th id="nuiteesHotels" class="border px-4 py-2" scope="col">
+                  Nuitées totales (en&nbsp;milliers)
+              </th>
+              <th id="evolutionHotels" class="border px-4 py-2" scope="col">
+                  Évolution des nuitées 2022/2019 (en&nbsp;%)
+              </th>
+              <th id="nuiteesCamping" class="border px-4 py-2" scope="col">
+                  Nuitées totales (en&nbsp;milliers)
+              </th>
+              <th id="evolutionCamping" class="border px-4 py-2" scope="col">
+                  Évolution des nuitées 2022/2019 (en&nbsp;%)
+              </th>
+              <th id="nuiteesAutre" class="border px-4 py-2" scope="col">
+                  Nuitées totales (en&nbsp;milliers)
+              </th>
+              <th id="evolutionAutre" class="border px-4 py-2" scope="col">
+                  Évolution des nuitées 2022/2019 (en&nbsp;%)
+              </th>
+              <th id="nuiteesTotal" class="border px-4 py-2" scope="col">
+                  Nuitées totales (en&nbsp;milliers)
+              </th>
+              <th id="evolutionTotal" class="border px-4 py-2" scope="col">
+                  Évolution des nuitées 2022/2019 (en&nbsp;%)
+              </th>
+          </tr>
+          <tr class="bg-gray-100">
+              <th id="bretagne" class="border px-4 py-2 font-bold text-left" scope="row">
+                  Bretagne
+              </th>
+              <td headers="hotels nuiteesHotels" class="border px-4 py-2">5&nbsp;173</td>
+              <td headers="hotels evolutionHotels" class="border px-4 py-2">-0,4</td>
+              <td headers="camping nuiteesCamping" class="border px-4 py-2">13&nbsp;641</td>
+              <td headers="camping evolutionCamping" class="border px-4 py-2">12,1</td>
+              <td class="border px-4 py-2">3&nbsp;040</td>
+              <td class="border px-4 py-2">1,0</td>
+              <td class="border px-4 py-2">21&nbsp;854</td>
+              <td class="border px-4 py-2">7,2</td>
+          </tr>
+          <tr>
+              <th id="cotesArmor" class="border px-4 py-2 text-left" scope="row">Côtes-d’Armor</th>
+              <td headers="hotels nuiteesHotels" class="border px-4 py-2">828</td>
+              <td headers="hotels evolutionHotels" class="border px-4 py-2">4,4</td>
+              <td headers="camping nuiteesCamping" class="border px-4 py-2">2&nbsp;332</td>
+              <td headers="camping evolutionCamping" class="border px-4 py-2">14,1</td>
+              <td class="border px-4 py-2">388</td>
+              <td class="border px-4 py-2">-8,5</td>
+              <td class="border px-4 py-2">3&nbsp;548</td>
+              <td class="border px-4 py-2">8,8</td>
+          </tr>
+          <tr class="bg-gray-100">
+              <th headers="finistere" class="border px-4 py-2 text-left" scope="row">Finistère</th>
+              <td headers="hotels nuiteesHotels" class="border px-4 py-2">1&nbsp;225</td>
+              <td headers="hotels evolutionHotels" class="border px-4 py-2">-4,7</td>
+              <td headers="camping nuiteesCamping" class="border px-4 py-2">4&nbsp;892</td>
+              <td headers="camping evolutionCamping" class="border px-4 py-2">10,4</td>
+              <td class="border px-4 py-2">1&nbsp;108</td>
+              <td class="border px-4 py-2">2,8</td>
+              <td class="border px-4 py-2">7&nbsp;224</td>
+              <td class="border px-4 py-2">6,3</td>
+          </tr>
+          <tr>
+              <th id="illeEtVilaine" class="border px-4 py-2 text-left" scope="row">Ille-et-Vilaine</th>
+              <td headers="hotels nuiteesHotels" class="border px-4 py-2">1&nbsp;857</td>
+              <td headers="hotels evolutionHotels" class="border px-4 py-2">-0,3</td>
+              <td headers="camping nuiteesCamping" class="border px-4 py-2">1&nbsp;367</td>
+              <td headers="camping evolutionCamping" class="border px-4 py-2">12,5</td>
+              <td class="border px-4 py-2">677</td>
+              <td class="border px-4 py-2">5,9</td>
+              <td class="border px-4 py-2">3&nbsp;901</td>
+              <td class="border px-4 py-2">5,0</td>
+          </tr>
+          <tr class="bg-gray-100">
+              <th id="morbihan" class="border px-4 py-2 text-left" scope="row">Morbihan</th>
+              <td headers="hotels nuiteesHotels" class="border px-4 py-2">1&nbsp;263</td>
+              <td headers="hotels evolutionHotels" class="border px-4 py-2">0,8</td>
+              <td headers="camping nuiteesCamping" class="border px-4 py-2">5&nbsp;050</td>
+              <td headers="camping evolutionCamping" class="border px-4 py-2">12,7</td>
+              <td class="border px-4 py-2">868</td>
+              <td class="border px-4 py-2">-0,4</td>
+              <td class="border px-4 py-2">7&nbsp;181</td>
+              <td class="border px-4 py-2">8,7</td>
+          </tr>
+          <tr>
+              <th id="france" class="border px-4 py-2 font-bold text-left" scope="row">
+                  France métropolitaine
+              </th>
+              <td headers="hotels nuiteesHotels" class="border px-4 py-2">125&nbsp;721</td>
+              <td headers="hotels evolutionHotels" class="border px-4 py-2">-0,9</td>
+              <td headers="camping nuiteesCamping" class="border px-4 py-2">135&nbsp;708</td>
+              <td headers="camping evolutionCamping" class="border px-4 py-2">7,5</td>
+              <td class="border px-4 py-2">58&nbsp;530</td>
+              <td class="border px-4 py-2">-1,7</td>
+              <td class="border px-4 py-2">319&nbsp;959</td>
+              <td class="border px-4 py-2">2,3</td>
+          </tr>
+      </tbody>
+  </table>
+
+  <a href="#" Aria-labelledby="Lien vers explication du tableau details titre">Lien vers explication du tableau</a>
+
+  <!-- <table
+      id="produit-tableau-figure2"
+      class="min-w-full table-auto border-collapse">
+      <caption class="text-lg font-semibold my-4 text-center">
+          Nuitées de la saison d’été 2022 et évolution entre 2019 et 2022
+          par département et type d’hébergement
+      </caption>
+      <thead class="bg-gray-200">
+          <tr>
+              <th class="border px-4 py-2" rowspan="2"></th>
+              <th class="border px-4 py-2" colspan="2" scope="col">
+                  Hôtels - avril-septembre&nbsp;2022
+              </th>
+              <th class="border px-4 py-2" colspan="2" scope="col">
+                  Camping - avril-septembre&nbsp;2022
+              </th>
+              <th class="border px-4 py-2" colspan="2" scope="col">
+                  Autres hébergements collectifs de tourisme -
+                  avril-septembre&nbsp;2022
+              </th>
+              <th class="border px-4 py-2" colspan="2" scope="col">
+                  Total - avril-septembre&nbsp;2022
+              </th>
+          </tr>
+          <tr>
+              <th class="border px-4 py-2">
+                  Nuitées totales (en&nbsp;milliers)
+              </th>
+              <th class="border px-4 py-2">
+                  Évolution des nuitées 2022/2019 (en&nbsp;%)
+              </th>
+              <th class="border px-4 py-2">
+                  Nuitées totales (en&nbsp;milliers)
+              </th>
+              <th class="border px-4 py-2">
+                  Évolution des nuitées 2022/2019 (en&nbsp;%)
+              </th>
+              <th class="border px-4 py-2">
+                  Nuitées totales (en&nbsp;milliers)
+              </th>
+              <th class="border px-4 py-2">
+                  Évolution des nuitées 2022/2019 (en&nbsp;%)
+              </th>
+              <th class="border px-4 py-2">
+                  Nuitées totales (en&nbsp;milliers)
+              </th>
+              <th class="border px-4 py-2">
+                  Évolution des nuitées 2022/2019 (en&nbsp;%)
+              </th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr class="bg-gray-100">
+              <th class="border px-4 py-2 font-bold text-left" scope="row">
+                  Bretagne
+              </th>
+              <td class="border px-4 py-2">5&nbsp;173</td>
+              <td class="border px-4 py-2">-0,4</td>
+              <td class="border px-4 py-2">13&nbsp;641</td>
+              <td class="border px-4 py-2">12,1</td>
+              <td class="border px-4 py-2">3&nbsp;040</td>
+              <td class="border px-4 py-2">1,0</td>
+              <td class="border px-4 py-2">21&nbsp;854</td>
+              <td class="border px-4 py-2">7,2</td>
+          </tr>
+          <tr>
+              <th class="border px-4 py-2 text-left" scope="row">Côtes-d’Armor</th>
+              <td class="border px-4 py-2">828</td>
+              <td class="border px-4 py-2">4,4</td>
+              <td class="border px-4 py-2">2&nbsp;332</td>
+              <td class="border px-4 py-2">14,1</td>
+              <td class="border px-4 py-2">388</td>
+              <td class="border px-4 py-2">-8,5</td>
+              <td class="border px-4 py-2">3&nbsp;548</td>
+              <td class="border px-4 py-2">8,8</td>
+          </tr>
+          <tr class="bg-gray-100">
+              <th class="border px-4 py-2 text-left" scope="row">Finistère</th>
+              <td class="border px-4 py-2">1&nbsp;225</td>
+              <td class="border px-4 py-2">-4,7</td>
+              <td class="border px-4 py-2">4&nbsp;892</td>
+              <td class="border px-4 py-2">10,4</td>
+              <td class="border px-4 py-2">1&nbsp;108</td>
+              <td class="border px-4 py-2">2,8</td>
+              <td class="border px-4 py-2">7&nbsp;224</td>
+              <td class="border px-4 py-2">6,3</td>
+          </tr>
+          <tr>
+              <th class="border px-4 py-2 text-left" scope="row">Ille-et-Vilaine</th>
+              <td class="border px-4 py-2">1&nbsp;857</td>
+              <td class="border px-4 py-2">-0,3</td>
+              <td class="border px-4 py-2">1&nbsp;367</td>
+              <td class="border px-4 py-2">12,5</td>
+              <td class="border px-4 py-2">677</td>
+              <td class="border px-4 py-2">5,9</td>
+              <td class="border px-4 py-2">3&nbsp;901</td>
+              <td class="border px-4 py-2">5,0</td>
+          </tr>
+          <tr class="bg-gray-100">
+              <th class="border px-4 py-2 text-left" scope="row">Morbihan</th>
+              <td class="border px-4 py-2">1&nbsp;263</td>
+              <td class="border px-4 py-2">0,8</td>
+              <td class="border px-4 py-2">5&nbsp;050</td>
+              <td class="border px-4 py-2">12,7</td>
+              <td class="border px-4 py-2">868</td>
+              <td class="border px-4 py-2">-0,4</td>
+              <td class="border px-4 py-2">7&nbsp;181</td>
+              <td class="border px-4 py-2">8,7</td>
+          </tr>
+          <tr>
+              <th class="border px-4 py-2 font-bold text-left" scope="row">
+                  France métropolitaine
+              </th>
+              <td class="border px-4 py-2">125&nbsp;721</td>
+              <td class="border px-4 py-2">-0,9</td>
+              <td class="border px-4 py-2">135&nbsp;708</td>
+              <td class="border px-4 py-2">7,5</td>
+              <td class="border px-4 py-2">58&nbsp;530</td>
+              <td class="border px-4 py-2">-1,7</td>
+              <td class="border px-4 py-2">319&nbsp;959</td>
+              <td class="border px-4 py-2">2,3</td>
+          </tr>
+      </tbody>
+  </table> -->
