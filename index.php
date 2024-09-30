8cfa1868bc8489bd8125dcb88ef5ada9fd16726a
Eliott <eliott.burkle@gmail.com>
2024-09-30 08:59:33 +0200

    Fix: add tailwindcss

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $page = isset($_GET['page']) ? $_GET['page'] : null;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./dist/main.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
